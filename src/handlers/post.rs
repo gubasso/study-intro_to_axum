68eb460b93d22f2d3b18a21d1c44fe5662fa62c8
Gustavo Basso <gubasso@eambar.net>
2023-11-12 16:48:44 -0300

    (up) Issue #extracting_json.

--- src/handlers/post.rs
+++ src/handlers/post.rs
@@ -1,7 +1,22 @@
+use axum::Json;
+use serde::{Serialize, Deserialize};
+
 pub async fn mirror_body_string(body: String) -> String {
     body
 }
 
-pub async fn mirror_body_json(body: String) -> String {
-    body
+#[derive(Deserialize, Debug)]
+pub struct MirrorJson {
+    message: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct MirrorJsonRespose {
+    respose: String,
+}
+
+
+pub async fn mirror_body_json(Json(mut body): Json<MirrorJson>) -> Json<MirrorJsonRespose> {
+    body.message.push_str(". Now a reponse.");
+    Json(MirrorJsonRespose { respose: body.message })
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ mod handlers;
 use std::net::SocketAddr;
 
 use axum::{routing::{get, post}, Router, Server};
-use handlers::{get::root, post::mirror_body_string};
+use handlers::{get::root, post::{mirror_body_string, mirror_body_json}};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root))
-        .route("/mirror_body_string", post(mirror_body_string));
+        .route("/mirror_body_string", post(mirror_body_string))
+        .route("/mirror_body_json", post(mirror_body_json));
 
     let addr = SocketAddr::from(([127,0,0,1], 3000));
     Server::bind(&addr)
